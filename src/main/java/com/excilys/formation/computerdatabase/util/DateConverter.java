299a3394032d5ce8263c35a112c7d9468237d30a
Zacaria <zc@42.am>
2016-03-10 17:34:42 +0100

    fix DateConverter error handler

--- src/main/java/com/excilys/formation/computerdatabase/util/DateConverter.java
+++ src/main/java/com/excilys/formation/computerdatabase/util/DateConverter.java
@@ -1,7 +1,5 @@
 package com.excilys.formation.computerdatabase.util;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.regex.Pattern;
@@ -10,6 +8,10 @@ public class DateConverter {
 	public static LocalDate stringToDate(String s){
 		//http://stackoverflow.com/questions/22061723/regex-date-validation-for-yyyy-mm-dd
 		Pattern dateFormat = Pattern.compile("^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$");
+		
+		if(!dateFormat.matcher(s).matches()){
+			return null;
+		}
 		
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		LocalDate date = LocalDate.parse(s, formatter);
