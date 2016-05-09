2a14ff2f7dc8e5dee9a2b58d10d57a25a1046cbb
Zacaria <zc@42.am>
2016-05-09 11:31:10 +0200

    [ UPDATE ] : error handling

--- webapp/src/main/java/com/excilys/formation/computerdatabase/controllers/exceptions/ExceptionsHandler.java
+++ webapp/src/main/java/com/excilys/formation/computerdatabase/controllers/exceptions/ExceptionsHandler.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ControllerAdvice
 public class ExceptionsHandler {
   private static final String ATTR_ERROR = "errors";
